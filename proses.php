018d390e0c212dcd1468669df7ad74b2b14dd633
yosesilalahi <43519393+yosesilalahi@users.noreply.github.com>
2018-09-28 10:07:14 +0700

    proses
    semua butuh proses

--- proses.php
+++ proses.php
@@ -1,4 +1,32 @@
 <?php
 
+	if(isset($_POST['kirim'])){
+		$username=$_POST['username']
+		$password=$_POST['password']
+		$user=array(
+					"user"=>"ujang",
+					"pass"=>"silalahi",);
+
+		if ($username==$user['user']&&$password==$user['pass']) {
+			# code...
+			echo "oke oce:"."<br>>";
+		}
+
 
 ?>
+
+
+
+		<form method="POST">
+		makanan<input type="checkbox" name="makanan[]" value="seblak"><br>
+		buah<input type="checkbox" name="buah[]" value="aple"><br>
+		laptop<input type="checkbox" name="laptop[]" value="toshiba"><br>
+		<input type="submit" name="submit" value="login">
+	</form>
+
+
+
+
+ ?>
+
+		
