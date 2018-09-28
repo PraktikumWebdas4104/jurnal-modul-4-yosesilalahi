e75e9fcecea8f11ff5abfea265369d4a3d0f81e7
yosesilalahi <43519393+yosesilalahi@users.noreply.github.com>
2018-09-28 09:53:11 +0700

    form awal
    untuk login pada tampilan awal

--- formawal.php
+++ formawal.php
@@ -1,5 +1,49 @@
-<?php
+<?php 
+session_start();
+error_reporting(0);
+	$user = array('user' =>'ujang','pass'=> 'silalahi');
+
+	if (isset($_POST['submit'])) {
+		# code...
+		if ($_POST['username']==$user['user'] && $_POST['password']==$user['pass']) {
+			# code...
+
+			$_sesion['username'] = $_POST['username'];
+			echo "login masuk, $_POST[username]";
+		}
+		else{
+			form_login();
+			echo "username/password salah";
+		}
+	}
+		else{
+			form_login();
+		}
+		
+
+		function form_login(){ ?>
+
+
+			<form action="<?php echo $_SERVER['php_SELF'];?>" method="POST">
+				<label for="username">username :</label>
+				<input type="text" name="username"><br><br>
+				<label for="password">password :</label>
+				<input type="password" name="password"><br><br>
+
+				<input type="submit" name="submit" value="login">
+
+			</form>
+
+			<h1>pilih</h1>
+		<form action="<?php echo $_SERVER['php_SELF'];?>" method="POST">
+		<input type="checkbox" name="makanan[' ']" value="seblak"><br>
+		<input type="checkbox" name='buah[]' value="aple"><br>
+		<input type="checkbox" name='laptop[]' value="toshiba"><br>
+		<input type="submit" name="submit" value="save">
 
 
 
-?>
+		<?php } ?>
+
+
+	
